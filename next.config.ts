aa7f94bb8e4a221d5ab190110b380e634d881c18
suji <1999ssu@gmail.com>
2026-07-06 15:09:05 +0900

    fix: add blob: to script-src in CSP to fix blob script blocking error

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com https://maps.gstatic.com",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: https://js.stripe.com https://maps.googleapis.com https://maps.gstatic.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "frame-src 'self' https://js.stripe.com https://hooks.stripe.com",
